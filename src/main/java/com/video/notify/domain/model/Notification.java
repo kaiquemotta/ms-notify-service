9146e27abcca116516a63898435ff83a5b7cd28e
Kaique Motta <kaique.motta@hotmail.com>
2025-04-20 01:23:48 -0300

    fix: config yml para testes

--- src/main/java/com/video/notify/domain/model/Notification.java
+++ src/main/java/com/video/notify/domain/model/Notification.java
@@ -1,4 +1,5 @@
 package com.video.notify.domain.model;
 
 
-public record Notification(String userId, String videoId, String status, String message,String subject, String email) {}
+public record Notification(String userId, String videoId, String status, String message, String subject, String email) {
+}
